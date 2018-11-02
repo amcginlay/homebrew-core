805051eff18f2cb284f37335df1842da71ee27ce
Alan McGinlay <amcginlay@users.noreply.github.com>
2018-11-02 16:19:26 +0000

    1.7.5

--- Formula/tomee-plus.rb
+++ Formula/tomee-plus.rb
@@ -1,8 +1,8 @@
 class TomeePlus < Formula
   desc "Everything in TomEE Web Profile and JAX-RS, plus more"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.7.4/apache-tomee-1.7.4-plus.tar.gz"
-  sha256 "be43806d65bde4fdd393e0d78f91f910f9bf2e468425738d817afe0fefaffbc3"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomee/tomee-1.7.5/apache-tomee-1.7.5-plus.tar.gz"
+  sha256 "b07f7a27f2d440c8254c56caf3d3637152fcee02"
 
   bottle :unneeded
 
